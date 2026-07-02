04ac94efb02d7d1d400a206ff0503af994d5c8f3
Nasir Ali <nasir604631@gmail.com>
2026-07-03 02:02:03 +0500

    Add files via upload

---
+++ 00_python_syntax.py
@@ -0,0 +1,28 @@
+
+# Topic: Python Syntax
+# Author: Nasir Ali
+# Description: My first python program 
+#Basic Python syntax is very simple 
+#This program print "Hello, World!" On the screen 
+
+print("Hello, World!")
+
+#Examples
+
+# Print numbers
+print(100)
+
+# Print multiple values
+print("Name:", "Ali")
+
+# Simple calculation
+print(10 + 20)
+print(5-3)
+print(6*7)
+print(10/2)
+
+# Printing on multiple lines
+print("Welcome")
+print("To Python")
+print("My name is Nasir Ali")
+print("My future journey to become a professional data analyst")
