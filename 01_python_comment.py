b17ef97b8904ec182b51d0c6bd8ee41d106ce91c
Nasir Ali <nasir604631@gmail.com>
2026-07-03 02:21:46 +0500

    Add files via upload

---
+++ 01_python_comment.py
@@ -0,0 +1,19 @@
+
+# Topic: Comments in Python
+# Description: How to write comments in Python
+# Author: Nasir Ali 
+
+# This is a single-line comment.
+print("Hello, World!")
+
+print("Python")  # This is an inline comment.
+
+# Comments are ignored by Python.
+# They are used to explain the code.
+
+# Example:
+name = "Ali"   # Store the user's name
+age = 27       # Store the user's age
+
+print(name)
+print(age)
