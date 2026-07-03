4db51e8475dd926ff380bff21deef57dc36470a5
Nasir Ali <nasir604631@gmail.com>
2026-07-03 15:30:57 +0500

    Add files via upload

---
+++ 03_Data_Types.py
@@ -0,0 +1,75 @@
+#Topic : Data Types
+# Description : Basic Data Types in Python 
+#Author : Nasir Ali 
+
+#What is Data Types 
+#Data Types define the type of data stored in a variable.
+
+#String(str)
+name = "Nasir"
+print(name)
+print(type(name))
+#output
+#<class 'str'>
+
+#Integer(int)
+age = 25
+print(age)
+print(type(age))
+
+#Float(float)
+height = 5.9
+print(height)
+print(type(height))
+
+#Boolean(bool)
+is_pass = True
+print(is_pass)
+print(type(is_pass))
+
+#List
+fruits = ["apple", "banana","mango"]
+print(fruits)
+print(type(fruits))
+
+marks = [77,86,81,92]
+print(marks)
+print(type(marks))
+
+#Tuple
+subjects = ("Computer","Mathematics","Physics")
+print(subjects)
+print(type(subjects))
+
+#Dictionary
+student = {
+    "name" : "Nasir",
+    "age" : 25,
+    "height" :5.9
+}
+print(student)
+print(type(student))
+
+#Set
+numbers = {1,2,3,5,6}
+print(numbers)
+print(type(numbers))
+
+#None Type 
+data = None
+print(data)
+print(type(data))
+
+
+#what i learned
+
+# String (str)
+# Integer (int)
+# Float (float)
+# Boolean (bool)
+# List
+# Tuple
+# Dictionary
+# Set
+# NoneType
+
