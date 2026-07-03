3ae088652517241db74d4b3e6dbbad7af68e6c40
Nasir Ali <nasir604631@gmail.com>
2026-07-03 06:34:15 +0500

    Add files via upload

---
+++ 02_Variables.py
@@ -0,0 +1,47 @@
+#Topic : Variables in Python 
+# Description: Learning variables in python 
+#Author: Nasir Ali
+
+ #what is variables 
+# Variables are used to store data
+
+#String Variable
+Name = "Ali"
+print(Name)
+
+#Integer Variable
+Age = 25
+print(Age)
+
+#Float Variables 
+Height = 5.9
+print(Height)
+
+#Boolean Variable
+Is_student = True 
+print(Is_student)
+
+#Multiple Variables 
+student_name = "Ali"
+student_age = 26
+student_marks = 89
+my_city = "Mutan" 
+my_country = "Pakistan"
+
+print(student_name)
+print(student_age)
+print(student_marks)
+print(my_city)
+print(my_country)
+
+#Multiple Assignment 
+x,y,z = 10,20,30
+print(x)
+print(y)
+print(z)
+
+#Same Value
+a = b = c = 70
+print(a)
+print(b)
+print (c)
