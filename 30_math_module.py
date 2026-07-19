1874e2a2d67e64b1c9ea076f1b20b6aa73832409
Nasir Ali <nasir604631@gmail.com>
2026-07-19 22:41:56 +0500

    Add files via upload

---
+++ 30_math_module.py
@@ -0,0 +1,73 @@
+
+#Topic: Math Module 
+#Description: Learning mathematical functions using Python's math modules.
+#Author: Nasir Ali 
+
+#What is the Math Module?
+#The math module is a built-in Python module that provides mathematical functions and constants.
+
+#Why do we use the math module?
+#we use the math module to perform advance mathematical calculations.
+
+import math 
+#1. sqrt()
+
+print(math.sqrt(64)) # square root 
+#2. pow()
+print(math.pow(2,5))  #2⁵
+#3. ceil()
+print(math.ceil(4.3))  # upper integer 
+#4. floor()
+print(math.floor(7.9))  # lower integer 
+#5. factorial()
+print(math.factorial(4)) # 5*4*3*2*1
+#6. pi
+print(math.pi)
+#7. e
+print(math.e)
+#8. fabs()
+print(math.fabs(-25)) # absolute value 
+#9. trunc()
+print(math.trunc(8.875))  # remove decimal 
+#10. gcd()
+print(math.gcd(20,30,25))  # greatest common divisor
+
+# Mini Practice 
+import math as m 
+print(m.factorial(8))
+print (m.floor(6.6))
+print(m.sqrt(144))
+from math import pi
+print(pi)
+
+
+# What I learned 
+
+# math module
+# sqrt()
+# pow()
+# ceil()
+# floor()
+# factorial()
+# pi
+# e
+# fabs()
+# trunc()
+# gcd()
+
+#Interview Questions
+
+#What is the math module?
+#Why do we use math.sqrt()?
+#What is the difference between ceil() and floor()?
+#What does factorial() return?
+#What is the value of pi?
+#What is the difference between pow() and ** operator?
+
+
+
+
+
+
+
+
